16668b3e360f09da73182451a87d9b467be71ea3
Gustavo Rosa <gustavorib.rosa@gmail.com>
2022-03-23 16:13:35 -0300

    Using context

--- spec/calculator/calculator_spec.rb
+++ spec/calculator/calculator_spec.rb
@@ -1,9 +1,17 @@
 require 'calculator'
 
 describe Calculator do
-    it 'use sum method for 2 numbers' do
-        calc = Calculator.new
-        result = calc.sum(5, 7)
-        expect(result).to eq(12)
-    end
+    context 'use sum method for 2 numbers' do
+        it 'positive numbers' do
+            calc = Calculator.new
+            result = calc.sum(5, 7)
+            expect(result).to eq(12)
+        end
+
+        it 'negative numbers' do
+            calc = Calculator.new
+            result = calc.sum(-5, 7)
+            expect(result).to eq(2)
+        end
+    end 
 end
